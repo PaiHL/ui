9e37ff60d3bcd763d135465bf933348a22f2c447
PaiHL <134986650+PaiHL@users.noreply.github.com>
2024-06-14 14:50:59 +0800

    Add files via upload

---
+++ link_linux_arm64.go
@@ -0,0 +1,10 @@
+//go:build !windows && !darwin
+// +build !windows,!darwin
+
+// 11 december 2015
+
+package ui
+
+// #cgo LDFLAGS: ${SRCDIR}/libui_linux_arm64.a -lm -ldl
+// #cgo pkg-config: gtk+-3.0
+import "C"
